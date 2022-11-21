6e82c1e30977113f54d04e42ca0e6427abbec9fe
mingshashan <aikongfu@163.com>
2022-11-21 16:26:56 +0800

    update practice demo

---
+++ 50.basic-c/01.linuxc-learn/assert/xassert.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void _Assert(char *mesg) {
+  /* print assertion message and abort */
+  fputs(mesg, stderr);
+  fputs(" -- assertion failed\n", stderr);
+  abort();
+}
---
+++ 50.basic-c/01.linuxc-learn/micro02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define CONCAT(a, b)  a##b
+#define HASH_HASH # ## #
+
+CONCAT(hello, hello);
+
+int main(void) {
+
+
+
+
+
+  
+  return 0;
+}
