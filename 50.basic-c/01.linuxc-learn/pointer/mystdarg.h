01b7e93e3d001fa1746fcd935515236473ea32d0
mingshashan <aikongfu@163.com>
2022-11-25 20:06:49 +0800

    update practice demo

---
+++ 50.basic-c/01.linuxc-learn/pointer/mystdarg.h
@@ -0,0 +1,19 @@
+/* mystdarg.h standard header */
+#ifndef MY_STDARG_H
+#define MY_STDARG_H
+
+/* type definitions */
+typedef char *va_list;
+
+/* macros */
+#define va_arg(ap, T) \
+  (*(T *)(((ap) += _Bnd(T, 3U)) - _Bnd(T, 3U)))
+
+#define va_end(ap) (void)0
+
+#define va_start(ap, A) \
+  (void)((ap) = (char *)&(A) + _Bnd(A, 3U))
+
+#define _Bnd(X, bnd) (sizeof(X) + (bnd) & ~(bnd))
+
+#endif
