2646e5e7915cc16e3ad3f15e9f3da9d37a07ed68
mingshashan <mingshashan@gmail.com>
2022-11-28 15:59:41 +0800

    update practice demo

--- 50.basic-c/01.linuxc-learn/pointer/strtok02.c
+++ 50.basic-c/01.linuxc-learn/pointer/strtok02.c
@@ -4,27 +4,37 @@
 char *my_strtok(char *str, const char *delim);
 char *my_strtok_r(char *str, const char *delim, char **saveptr);
 
-int is_delim(char ch, const char *delim) {
+
+int match_delim(char ch, const char *delim) {
   while (*delim != '\0') {
     if (ch == *delim) {
       return 1;
     }
     delim++;
   }
+  
   return 0;
 }
 
+// my strtok implement
 char *my_strtok(char *str, const char *delim) {
+  
+  // backup str for the next strtok
   static char *backup;
-  if(!str) {
+  
+  // check str is NULL
+  if (*str) {
+    backup = str;
+  } else {
     str = backup;
   }
-  if(!str) {
+  
+  if (!(*str)) {
     return NULL;
   }
-  
+
   while (1) {
-    if (is_delim(*str, delim)) {
+    if (match_delim(*str, delim)) {
       str++;
       continue;
     }
@@ -36,30 +46,45 @@ char *my_strtok(char *str, const char *delim) {
 
   char *ret = str;
   while (1) {
-    if (*str == '\0') {
-      backup = str;
-      return ret;
-    }
-    if (is_delim(*str, delim)) {
+    if (match_delim(*str, delim)) {
       *str = '\0';
-      backup = str+1;
+      backup = str + 1;
       return ret;
     }
-    str++;
-  }
-  
-  
+    if (*str == '\0') {
+      return NULL;
+    }
 
+    *delim++;
+  }
+
+  return NULL;
 }
 
-int main(void) {
+
+void test_my_strtok() {
   char str[] = "root:x::0:root:/root:/bin/bash:";
   char *token;
 
-  token = strtok(str, "%:%");
+  token = my_strtok(str, "%:%");
   printf("%s\n", token);
-  while ((token = strtok(NULL, "%:%"))!= NULL) {
+  while ((token = my_strtok(NULL, "%:%"))!= NULL) {
     printf("%s\n", token);
   }
+}
+
+void test_my_strtok_r() {
+  char *str = "a/bbb///cc;xxx:yyy:";
+  char *d1 = ":;";
+  char *d2 =  "/";
+
+}
+
+
+int main(void) {
+  
+  test_my_strtok();
+  test_my_strtok_r();
+
   return 0;
 }
