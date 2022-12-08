b20254e5ce24d2d21649094e86fe8f087dbf0be0
mingshashan <mingshashan@gmail.com>
2022-12-08 14:07:39 +0800

    update practice demo

---
+++ 41.algorithm/00.structure/doublelinkedlist/doublelinkedlist.h
@@ -0,0 +1,20 @@
+#ifndef DOUBLELINKEDLIST_H
+#define DOUBLELINKEDLIST_H
+
+typedef struct node *link;
+struct node {
+  unsigned char item;
+  link prev, next;
+};
+
+link make_node(unsigned char item);
+void free_node(link p);
+link search(unsigned char key);
+void insert(link p);
+void delete(link p);
+void traverse(void (*visit)(link));
+void destroy(void);
+void enqueue(link p);
+link dequeue(void);
+
+#endif
