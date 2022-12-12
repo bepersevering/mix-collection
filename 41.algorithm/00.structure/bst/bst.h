c226dc40e794ba8c2d353765fc379d911f5f6ab2
mingshashan <aikongfu@163.com>
2022-12-12 18:23:10 +0800

    update practice demo

--- 41.algorithm/00.structure/bst/bst.h
+++ 41.algorithm/00.structure/bst/bst.h
@@ -1,5 +1,16 @@
 #ifndef BST_H
 #define BST_H
 
+typedef struct node *link;
+struct node {
+  unsigned char item;
+  link left, right;
+};
+
+link search(link t, unsigned char key);
+link insert(link t, unsigned char key);
+link delete(link t, unsigned char key);
+void print_tree(link t);
+  
 
 #endif // !BST_H
