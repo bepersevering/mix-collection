e55561996fc7f5f3066b15c51125b0d1c7a2cc76
mingshashan <mingshashan@gmail.com>
2022-12-14 11:26:22 +0800

    udpate practice demo

--- 41.algorithm/00.structure/bst/bst_main.c
+++ 41.algorithm/00.structure/bst/bst_main.c
@@ -6,31 +6,32 @@
 #define RANGE 100
 #define N 6
 
-void print_item(link p) {
+void print_item(link p)
+{
     printf("%d", p->item);
 }
 
-int main(void) {
+int main(void)
+{
     int i, key;
     link root = NULL;
     srand(time(NULL));
-    for (i = 0; i < N; i++) {
+    for (i = 0; i < N; i++)
         root = insert(root, rand() % RANGE);
-    }
-
     printf("\t\\tree");
     print_tree(root);
     printf("\n\n");
-
-    while (root) {
+    while (root)
+    {
         key = rand() % RANGE;
-        if (search(root, key)) {
-            printf("delete %d in tree\n", key);
-            root = delete(root, key);
+        if (search(root, key))
+        {
+            printf("delete %d in tree\n",
+                   key);
+            root = delete (root, key);
             printf("\t\\tree");
             print_tree(root);
             printf("\n\n");
         }
     }
-
 }
