11360dd60114fd290b1fb43f2454a1801df8dae9
mingshashan <mingshashan@gmail.com>
2023-03-17 16:50:49 +0800

    update practice java

---
+++ 52.java/01.basic/Foo2.java
@@ -0,0 +1,9 @@
+public class Foo2 {
+
+	public static void main(String[] args){
+		boolean flag = true;
+		if (flag) System.out.println("Hello,java!");
+		if (flag == true) System.out.println("Hello, JVM!");
+	}
+
+}
