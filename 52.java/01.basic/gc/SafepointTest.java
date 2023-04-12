e4993935f617a28e2f3ae647254a672e869ccfa8
mingshashan <mingshashan@gmail.com>
2023-04-12 18:10:55 +0800

    feat: update gc practice

---
+++ 52.java/01.basic/gc/SafepointTest.java
@@ -0,0 +1,26 @@
+
+import java.lang.Math;
+import java.lang.Object;
+import java.lang.Thread;
+
+public class SafepointTest {
+	
+	static double sum = 0;
+
+	public static void foo() {
+		for (int i = 0; i < 0x77777777; i++) {
+			sum += Math.sqrt(i);
+		}
+	}
+
+	public static void bar() {
+		for (int i = 0; i < 50_000_000; i++) {
+			new Object().hashCode();
+		}
+	}
+
+	public static void main(String[] args) {
+		new Thread(SafepointTest::foo).start();
+		new Thread(SafepointTest::bar).start();
+	}
+}
