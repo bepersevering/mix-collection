fb75abc3a84be225ff0b6bdf073089472f1330de
mingshashan <aikongfu@163.com>
2024-01-23 18:15:01 +0800

    update

--- 51.socket/tinyhttpd.c
+++ 51.socket/tinyhttpd.c
@@ -5,16 +5,16 @@
 #include <unistd.h>
 
 #define PORT 18080
-#define BUFFER_SIZE = 1024
+#define BUFFER_SIZE 1024
 
 /**
  * param client_socket 客户端套接字文件描述符ID
  */
-void handle_requese(int client_socket) {
+void handle_request(int client_socket) {
   char buffer[BUFFER_SIZE];
   ssize_t bytes_received;
   // 从客户端接收请求
-  bytes_received recv(client_socket, buffer, BUFFER_SIZE, 0);
+  bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
   if (bytes_received < 0) {
     perror("Error receiving data from client");
     return;
@@ -31,25 +31,25 @@ void handle_requese(int client_socket) {
   close(client_socket);
 }
 
-int main(int argc, **char agrv) {
+int main(int argc, const char **agrv) {
 
   int server_socket, client_socket;
   struct sockaddr_in server_address, client_address;
-  socketlen_t client_address_len = sizeof(client_address);
+  socklen_t client_address_len = sizeof(client_address);
 
   // 创建套接字
-  server_socket = socket(AF_INET, SOCKET_STREAM, 0);
+  server_socket = socket(AF_INET, SOCK_STREAM, 0);
   if (server_socket < 0) {
     perror("Error creating server socket");
     exit(EXIT_FAILURE);
   }
 
-  server_socket.sin_family = AF_INET;
-  server_socket.sin_addr.s_addr = INADDR_ANY;
-  server_socket.sin_port = htons(PORT);
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = INADDR_ANY;
+  server_address.sin_port = htons(PORT);
 
   // 绑定套接字
-  if (bind(server_socket, struct(sockaddr *) & server_address,
+  if (bind(server_socket, (struct sockaddr *)&server_address,
            sizeof(server_address)) < 0) {
     perror("Error binding server socket");
     exit(EXIT_FAILURE);
