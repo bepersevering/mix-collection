a38f68ce33f1ad9299929e572626e1875a716203
bepersevering <mingshashan@gmail.com>
2024-05-22 16:52:39 +0800

    update

---
+++ 51.socket/learn/echo_client.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 1024
+void error_handling(char *msg);
+
+
+int main(int argc, char **argv) {
+
+  int sockfd;
+  char msg[BUF_SIZE];
+  int str_len;
+  struct sockaddr_in servaddr;
+
+  if (argc != 3) {
+    printf("Usage: %s <IP> <port>\n", argv[0]);
+    exit(1);
+  }
+
+  sockfd = socket(PF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+    error_handling("socket() error");
+  }
+
+  bzero(&servaddr, sizeof(servaddr));
+
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = inet_addr(argv[1]);
+  servaddr.sin_port = htons(atoi(argv[2]));
+
+  if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
+    error_handling("connect() error");
+  } else {
+    puts("Connected ......");
+  }
+
+  while(1) {
+    fputs("Input message(Q to quit):", stdout);
+    fgets(msg, BUF_SIZE, stdin);
+
+    if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")) {
+      break;
+    }
+
+    write(sockfd, msg, strlen(msg));
+    str_len = read(sockfd, msg, BUF_SIZE - 1);
+    msg[str_len] = 0;
+    printf("Message from server: %s", msg);
+  }
+
+  close(sockfd);
+  return 0;
+}
+
+void error_handling(char *msg) {
+  fputs(msg, stderr);
+  fputc('\n', stderr);
+  exit(1);
+}
