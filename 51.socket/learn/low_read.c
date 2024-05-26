67ff8cc2137587cdfe055c045f3026b4f5616683
mingshashan <mingshashan>
2024-05-26 10:29:21 +0800

    update

--- 51.socket/learn/low_read.c
+++ 51.socket/learn/low_read.c
@@ -3,31 +3,33 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define BUF_SIZE 1024
+
 void error_handling(char *msg);
+
 
 int main(void) {
   int fd;
 
-  char buf[] = "Let's go!\n";
+  char buf[BUF_SIZE];
 
-  fd = open("data.txt", O_CREAT|O_WRONLY|O_TRUNC);
+  fd = open("data.txt", O_RDONLY);
 
   if (fd == -1) {
     error_handling("open() error!");
   }
 
-  printf("file descriptor: %d \n", fd);
-
-  if (write(fd, buf, sizeof(buf)) == -1) {
-    error_handling("write() error!");
-  }
+  printf("file data: %s\n", buf);
 
   close(fd);
   return 0;
 }
+
 
 void error_handling(char *msg) {
   fputs(msg, stderr);
   fputc('\n', stderr);
   exit(1);
 }
+
+
