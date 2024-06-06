5e69d28ca0cd8a6e1200dc43f552a6a3a4cfae01
bepersevering <mingshashan@gmail.com>
2024-06-06 20:30:20 +0800

    update

---
+++ 51.socket/learn/desto.c
@@ -0,0 +1,18 @@
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(void) {
+  FILE *fp;
+
+  int fd = open("data.dat", O_RDONLY | O_WRONLY | O_TRUNC);
+
+  if (fd == -1) {
+    fputs("file open error", stdout);
+    return -1;
+  }
+
+  fp = fdopen(fd, "w");
+  fputs("Network C programming \n", fp);
+  fclose(fp);
+  return 0;
+}
