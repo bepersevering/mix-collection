99ed9a8a66dafd2b1bd940b3494e2f9d5c9ad323
bepersevering <mingshashan@gmail.com>
2024-06-24 19:55:49 +0800

    update

--- 51.socket/learn/poll_serv2.c
+++ 51.socket/learn/poll_serv2.c
@@ -12,11 +12,38 @@
 #define BUF_SIZE 10
 #define CLIENT_SIZE 96
 
-int createSocket() {
+int createSocket(int port) {
   int listen_fd = -1;
   struct sockaddr_in servaddr;
 
   if (-1 == (listen_fd = socket(PF_INET, SOCK_STREAM, 0))) {
-
+    fprintf(stderr, "create socket error: %d, %s\n", errno, strerror(errno));
+    exit(1);
   }
+
+  int reuseaddr = 1;
+  if (-1 == (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, 
+      &reuseaddr, sizeof(reuseaddr)))) {
+    fprintf(stderr, "setsockopt error: %d, %s\n", errno, strerror(errno));
+    exit(1);
+  }
+  
+  bzero(&servaddr, sizeof(servaddr));
+
+  servaddr.sin_family = PF_INET;
+  servaddr.sin_port = htons(port);
+
+  inet_pton(PF_INET, "0.0.0.0", &servaddr.sin_addr);
+
+  if (-1 == bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr))) {
+    fprintf(stderr, "bind error: %d, %s\n", errno, strerror(errno));
+    exit(1);
+  }
+
+  if (-1 == listen(listen_fd, 5)){
+    fprintf(stderr, "listen: %d, %s\n", errno, strerror(errno));
+    exit(1);
+  }
+
+  return listen_fd;
 }
