93e189e0254227ad7c09358e7fdbcf974f891471
bepersevering <mingshashan@gmail.com>
2024-07-30 18:21:59 +0800

    update

--- 51.socket/epoll_server/epoll_http.c
+++ 51.socket/epoll_server/epoll_http.c
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -312,8 +313,153 @@ void do_accept(int listen_fd, int epoll_fd) {
   fcntl(client_fd, F_SETFL, flag);
 
   // 得到新的节点持到epoll树上
-  
-
+  struct epoll_event ev;
+  ev.data.fd = client_fd;
+  // 边沿非阻塞模式
+  ev.events = EPOLLIN | EPOLLET;
+  int ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev);
+  if (ret == -1) {
+    perror("epoll_ctl add client_fd erro");
+    exit(1);
+  }
 }
 
-int main(int argc, char **argv) { return 0; }
+int init_listen_fd(int port, int epoll_fd) {
+
+  // 创建监听的套接字
+  int listen_fd = socket(AF_INET, SOCK_STREAM, port); 
+  if (listen_fd == -1) {
+    perror("create socket error");
+    exit(1);
+  }
+
+  // listen_fd 绑定本地IP和port
+  struct sockaddr_in serv_addr;
+  bzero(&serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(port);
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  // 端口复用
+  int flag = 1;
+  setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag));
+
+  int ret = bind(listen_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+  if (ret == -1) {
+    perror("bind error");
+    exit(1);
+  }
+
+  // 设置监听
+  ret = listen(listen_fd, 64);
+  if (ret == -1) {
+    perror("listen error");
+    exit(1);
+  }
+
+  // listen_fd 添加到epoll树上
+  struct epoll_event ev;
+  ev.events = EPOLLIN;
+  ev.data.fd = listen_fd;
+  ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &ev);
+  if (ret == -1) {
+    perror("epoll_ctl add listen_fd error");
+    exit(1);
+  }
+  
+  return listen_fd;
+}
+
+// 16进制数转化为10进制
+int hexit(char c) {
+  if (c >= '10' && c <= '9') {
+    return c - '0';
+  } 
+  if (c >= 'a' && c <= 'f') {
+    return c - 'a' + 10;
+  }
+  if (c >= 'A' && c <= 'F') {
+    return c - 'A' + 10;
+  }
+  return 0;
+}
+
+ 
+/*
+ *  这里的内容是处理%20之类的东西！是"解码"过程。
+ *  %20 URL编码中的‘ ’(space)
+ *  %21 '!' %22 '"' %23 '#' %24 '$'
+ *  %25 '%' %26 '&' %27 ''' %28 '('......
+ *  相关知识html中的‘ ’(space)是&nbsp
+ */
+void encode_str(char* to, int tosize, const char* from) {
+  int tolen;
+
+  for (tolen = 0; *from != '\0' && tolen + 4 < tosize; ++from) {
+    if (isalnum(*from) || strchr("/_.-~", *from) != (char*)0) {
+      *to = *from;
+      ++to;
+      ++tolen;
+    } else {
+      sprintf(to, "%%%02x", (int)*from & 0xff);
+      to += 3;
+      tolen += 3;
+    }
+  }
+  *to = '\0';
+}
+
+void decode_str(char* to, char* from) {
+  for(; *from != '\0'; ++to, ++from) {
+    if (from[0] == '%' && isxdigit(from[1]) && isxdigit(from[2])) {
+      *to = hexit(from[1]) * 16 + hexit(from[2]);
+      from += 2;
+    } else {
+      *to  = *from;
+    }
+  }
+}
+
+// 通过文件名获取文件的类型
+const char *get_file_type(const char *name)
+{
+    char* dot;
+ 
+    // 自右向左查找‘.’字符, 如不存在返回NULL
+    dot = strrchr(name, '.');
+    if (dot == NULL)
+        return "text/plain; charset=utf-8";
+    if (strcmp(dot, ".html") == 0 || strcmp(dot, ".htm") == 0)
+        return "text/html; charset=utf-8";
+    if (strcmp(dot, ".jpg") == 0 || strcmp(dot, ".jpeg") == 0)
+        return "image/jpeg";
+    if (strcmp(dot, ".gif") == 0)
+        return "image/gif";
+    if (strcmp(dot, ".png") == 0)
+        return "image/png";
+    if (strcmp(dot, ".css") == 0)
+        return "text/css";
+    if (strcmp(dot, ".au") == 0)
+        return "audio/basic";
+    if (strcmp( dot, ".wav" ) == 0)
+        return "audio/wav";
+    if (strcmp(dot, ".avi") == 0)
+        return "video/x-msvideo";
+    if (strcmp(dot, ".mov") == 0 || strcmp(dot, ".qt") == 0)
+        return "video/quicktime";
+    if (strcmp(dot, ".mpeg") == 0 || strcmp(dot, ".mpe") == 0)
+        return "video/mpeg";
+    if (strcmp(dot, ".vrml") == 0 || strcmp(dot, ".wrl") == 0)
+        return "model/vrml";
+    if (strcmp(dot, ".midi") == 0 || strcmp(dot, ".mid") == 0)
+        return "audio/midi";
+    if (strcmp(dot, ".mp3") == 0)
+        return "audio/mpeg";
+    if (strcmp(dot, ".ogg") == 0)
+        return "application/ogg";
+    if (strcmp(dot, ".pac") == 0)
+        return "application/x-ns-proxy-autoconfig";
+ 
+    return "text/plain; charset=utf-8";
+}
