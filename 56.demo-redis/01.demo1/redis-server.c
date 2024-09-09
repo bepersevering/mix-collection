c3add69af30398ec4f12d7a88e8f785af1fc1ec2
bepersevering <mingshashan@gmail.com>
2024-09-09 18:36:26 +0800

    update

--- 56.demo-redis/01.demo1/redis-server.c
+++ 56.demo-redis/01.demo1/redis-server.c
@@ -1,11 +1,13 @@
+#include <error.h>
+#include <fcntl.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/epoll.h>
-#include <netinet/in.h>
-#include <error.h>
-#include <fcntl.h>
+#include <unistd.h>
+
+#define ULONG_MAX 0xFFFFFFFFUL
 
 #define PORT 6369
 #define MAX_EVENTS 10
@@ -13,8 +15,8 @@
 #define TABLE_SIZE 1000
 
 /**
-* data entry
-*/
+ * data entry
+ */
 typedef struct Entry {
   char *key;
   char *value;
@@ -26,5 +28,43 @@ Entry *hashTable[TABLE_SIZE];
 
 /** hash function */
 unsigned int hash(const char *key) {
+  unsigned long int hashValue = 0;
+  int i = 0;
+  while (hashValue < ULONG_MAX && i < strlen(key)) {
+    hashValue = hashValue * 37 + key[i];
+    i++;
+  }
 
+  return hashValue * TABLE_SIZE;
+}
+
+// insert K-V
+void set(const char *key, const char *value) {
+  unsigned int index = hash(key);
+
+  Entry *entry = hashTable[index];
+
+  // if exist then cover
+  while (entry != NULL) {
+    if (strcmp(entry->key, key) == 0) {
+      free(entry->value);
+      entry->value = strdup(value);
+      return;
+    }
+    entry = entry->next;
+  }
+  // if not exist then set net k,v
+  Entry *newEntry = malloc(sizeof(Entry));
+  newEntry->key = strdup(key);
+  newEntry->value = strdup(value);
+  newEntry->next = hashTable[index];
+  hashTable[index] = newEntry;
+}
+
+
+char *get(const char* key) {
+  unsigned int index = hash(key);
+
+
+  return NULL;
 }
