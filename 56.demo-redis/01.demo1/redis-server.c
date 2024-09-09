0154421b514aebf35d88a918c3955525e3168d6c
bepersevering <mingshashan@gmail.com>
2024-09-09 22:31:29 +0800

    update

--- 56.demo-redis/01.demo1/redis-server.c
+++ 56.demo-redis/01.demo1/redis-server.c
@@ -144,13 +144,85 @@ void handle_client_request(int client_fd) {
       write(client_fd, "(nil)", 6);
     }
   } else if (sscanf(buffer, "DELETE %s", key) == 1) {
-    delete(key);
+    delete (key);
     write(client_fd, "OK", 3);
   } else {
     write(client_fd, "Unknown command\n", 16);
   }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
+  int server_fd, epoll_fd;
 
+  struct epoll_event ev, events[MAX_EVENTS];
+
+  struct sockaddr_in addr;
+
+  // create socket
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (server_fd == -1) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+  set_nonblocking(server_fd);
+
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(PORT);
+  addr.sin_addr.s_addr = INADDR_ANY;
+
+  if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+    perror("bind");
+    close(server_fd);
+    exit(EXIT_FAILURE);
+  }
+
+  // listen
+  if (listen(server_fd, SOMAXCONN) == -1) {
+    perror("listen");
+    close(server_fd);
+    exit(EXIT_FAILURE);
+  }
+
+  // create epoll
+  epoll_fd = epoll_create(1);
+  if (epoll_fd == -1) {
+    perror("epoll_create");
+    close(server_fd);
+    exit(EXIT_FAILURE);
+  }
+
+  // add server socket to epoll
+  ev.events = EPOLLIN;
+  ev.data.fd = server_fd;
+
+  if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev) == -1) {
+    perror("epoll_ctrl: server_fd");
+    close(server_fd);
+    close(epoll_fd);
+    exit(EXIT_FAILURE);
+  }
+  printf("Redis server srtarted on port %d\n", PORT);
+
+  while (1) {
+    int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+    if (nfds == -1) {
+      perror("epoll_wait");
+      close(server_fd);
+      close(epoll_fd);
+      exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < nfds; i++) {
+      if (events[i].data.fd = server_fd) {
+        // accept new clietn
+        int clietn_fd = accept(server_fd, NULL, NULL);
+        if (clietn_fd == -1) {
+          perror("accept");
+          continue;
+        }
+      }
+    }
+  }
 }
