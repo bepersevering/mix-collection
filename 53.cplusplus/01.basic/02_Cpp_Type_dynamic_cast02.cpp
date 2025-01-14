40eaf779fae7f2bf968466b8e95120cf55dfefa0
bepersevering <mingshashan@gmail.com>
2025-01-14 18:53:44 +0800

    update

--- 53.cplusplus/01.basic/02_Cpp_Type_dynamic_cast02.cpp
+++ 53.cplusplus/01.basic/02_Cpp_Type_dynamic_cast02.cpp
@@ -13,4 +13,18 @@ public:
   void show() { std::cout << "Derived class method" << std::endl; }
 };
 
-int main() { return 0; }
+int main() { 
+    Derived derived_obj;
+    Base &ref_base = derived_obj;
+
+    try {
+        // 将基类引用转换为派生对象
+        Derived& ref_derived = dynamic_cast<Derived&> (ref_base);
+        ref_derived.show(); //成功转换，调用派生类方法
+
+    } catch (const std::bad_cast& e) {
+        std::cout << "Dynamic cast failed: " << e.what() << std::endl;
+    }
+    
+    return 0; 
+}
